58d0195f7d09932eed9593c309a36702763bf2f3
marirs <marirs@gmail.com>
2021-11-17 15:44:40 +0530

    clippy

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,15 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Regex error: {0}")]
-    UtfError(#[from] regex::Error),
+    Regex(#[from] regex::Error),
     #[error("PE error: {0}")]
-    PeLiteError(#[from] pelite::Error),
+    PeLite(#[from] pelite::Error),
     #[error("LightGBM error: {0}")]
-    LightGBMError(#[from] lightgbm::Error),
+    LightGBM(#[from] lightgbm::Error),
     #[error("IO error: {0}")]
-    IoError(#[from] std::io::Error),
+    Io(#[from] std::io::Error),
     #[error("UTF8 error: {0}")]
-    Utf8Error(#[from] std::str::Utf8Error),
+    Utf8(#[from] std::str::Utf8Error),
     #[error("{0}")]
     Generic(String),
 }
